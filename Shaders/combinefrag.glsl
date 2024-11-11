3500d4fb72d9cedfeb37e1aa576a74274d8ac83c
c1009080 <c1009080@newcastle.ac.uk>
2024-11-11 11:10:38 +0000

    Fixed 15 in bumpvertex, has commented extra code with it

--- Shaders/combinefrag.glsl
+++ Shaders/combinefrag.glsl
@@ -15,8 +15,32 @@ void main(void){
 	vec3 light = texture(diffuseLight, IN.texCoord).xyz;
 	vec3 specular = texture(specularLight, IN.texCoord).xyz;
 
-	fragColour.xyz  = diffuse *0.1;
+	fragColour.xyz  = diffuse * 0.1;
 	fragColour.xyz += diffuse * light;
 	fragColour.xyz += specular;
 	fragColour.a  = 1.0;
 }
+/*
+#version 330 core
+
+uniform sampler2D diffuseTex;
+uniform sampler2D diffuseLight;
+uniform sampler2D specularLight;
+
+in Vertex {
+    vec2 texCoord;
+} IN;
+
+out vec4 fragColour;
+
+void main(void) {
+    vec3 diffuse = texture(diffuseTex, IN.texCoord).xyz;
+    vec3 light = texture(diffuseLight, IN.texCoord).xyz;
+    vec3 specular = texture(specularLight, IN.texCoord).xyz;
+
+    fragColour.xyz = diffuse * 0.1; // ambient
+    fragColour.xyz += diffuse * light; // lambert
+    fragColour.xyz += specular; // Specular
+    fragColour.a = clamp(fragColour.r + fragColour.g + fragColour.b, 0.0, 1.0);
+
+}*/
